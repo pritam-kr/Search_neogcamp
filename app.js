251347be67f684a3fbc1396a18d5fbb21cc67ccb
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-09-17 22:17:48 +0530

    still working on it

---
+++ app.js
@@ -0,0 +1,223 @@
+const database = {
+  team: [
+    {
+      title: "Tanay Pratap",
+      img: "./team/tanay.jpg",
+      job: "Senior Software Engineer at Microsoft",
+      des: "Teacher to thousands",
+      link: "https://www.linkedin.com/in/tanaypratap/",
+    },
+    {
+      title: "Akanksha Choudhary",
+      img: "./team/akanksha.jpg",
+      job: "Engineer at Viacom18",
+      des: "Cracking the JS interviews",
+      link: "https://www.linkedin.com/in/akankshachoudhary",
+    },
+    {
+      title: "Sushil Tripathi",
+      img: "./team/sushil.jpg",
+      job: "Software Engineer at Microsoft",
+      des: "Data Structures and Algorithms using JavaScript",
+      link: "https://www.linkedin.com/in/sushilt3",
+    },
+    {
+      title: "Pavithra Kodmad",
+      img: "./team/pavithra.jpg",
+      job: "Frontend Dev at Atlassian",
+      des: "Web Performance",
+      link: "https://www.linkedin.com/in/pavithra-kodmad",
+    },
+    {
+      title: "Vikram Santhalia",
+      img: "./team/vikram.jpg",
+      job: "UX Engineer at Google",
+      des: "Intro to Angular and CSS Animations in Depth",
+      link: "https://www.linkedin.com/in/vikramsanthalia",
+    },
+    {
+      title: "Rajat Kumar",
+      img: "./team/rajat.jpg",
+      job: "Engineer at Netflix",
+      des: "Understanding bundlers better by building your own",
+      link: "https://www.linkedin.com/in/rajatkum",
+    },
+    {
+      title: "Manjula Dube",
+      img: "./team/manjula.jpg",
+      job: "Engineer at Vanguard",
+      des: "Web Accessibility 101",
+      link: "Manjula Dube",
+    },
+    {
+      title: "Ruphaa Ganesh",
+      img: "./team/ruphaa.jpg",
+      job: "Developer at DeepSourceHQ",
+      des: "VueJS: First Look",
+      link: "https://www.linkedin.com/in/ruphaaganesh",
+    },
+    {
+      title: "Kushagra Gour",
+      img: "./team/kushagra.jpg",
+      job: "Frontend Lead at PushOwl",
+      des: "Knowing CSS Best Practices",
+      link: "https://www.linkedin.com/in/chinchang",
+    },
+  ],
+  levelzero: [
+    {
+      title: "First CLI App: Intro to programming",
+      link: "https://youtu.be/_L-UszPmy2A",
+      img: "./yimg/1.png",
+      des: "Welcome to neogcamp's first class. This is the most basic class you'll find on programming. We will use JavaScript to code. Using nodeJS we will write a CLI (Command Line Interface) program. We'll then use https://repl.it​ to host it.",
+    },
+    {
+      title: "Getting started with Git, VSCode and Hosting",
+      link: "https://youtu.be/MMEIVh49pS8",
+      img: "./yimg/2.png",
+      des: "This session will set you up for the next class where we'll develop our very own portfolio website and blog. The blog part will be an assignment but I'll help you with that.",
+    },
+    {
+      title: "Getting started with HTML5",
+      link: "https://youtu.be/No8qdcVYiQw",
+      img: "./yimg/3.png",
+      des: "You don't know anything about web development? Good, you have come to the right place. Not only you'll learn the language of the web: HTML, you'll learn how to make your site pretty with CSS.",
+    },
+    {
+      title: "Getting started with CSS",
+      link: "https://youtu.be/u6_a0d94A1Q",
+      img: "./yimg/4.png",
+      des: "In this course we will learn how to make your site pretty with CSS.",
+    },
+    {
+      title: "JavaScript in the browser!",
+      link: "https://youtu.be/yLZazznWoAs",
+      img: "./yimg/5.png",
+      des: "This time we will use both things to create a dynamic app in a web browser. You'll learn about DOM scripting i.e. JS in the browser, calling servers, and getting data from there.",
+    },
+    {
+      title: "First React App: Introduction to ReactJS library",
+      link: "https://youtu.be/KUJsaM-hAjs",
+      img: "./yimg/6.png",
+      des: "Now we use ReactJS, the most in-demand JS library today to create a dynamic app in the browser. We will be looking at the basics of React, JSX, useState, onClick handler, etc.",
+    },
+  ],
+  links: [
+    {
+      title: "Level zero all concept",
+      link: "https://reminiscent-cold-fd5.notion.site/All-levelZero-Concepts-under-one-roof-8be7cf78c82c4489b061ff0b77e518cb",
+    },
+    {
+      title: "Interview question",
+      link: "https://github.com/neogcamp/build/blob/main/neog%20interview%20sample%20questions/interview-questions.md",
+    },
+    {
+      title: "Interview question solutions",
+      link: "https://interview-question.netlify.app/",
+    },
+    {
+      title: "Logic build question",
+      link: "https://github.com/neogcamp/build/tree/main/build-logic",
+    },
+    {
+      title: "neogcamp addmission",
+      link: "https://admissions.neog.camp/",
+    },
+    {
+      title: "neogcamp discord server",
+      link: "https://bit.ly/tanaydiscord",
+    },
+    {
+      title: "neogcamp youtube channel",
+      link: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA",
+    },
+    {
+      title: "levelZero playlist",
+      link: "https://www.youtube.com/playlist?list=PLzvhQUIpvvuj5KPnyPyWsvgyzNkX_ACPA",
+    },
+    {
+      title: "mark15",
+      link: "https://neog.camp/qualifier/point-system",
+    },
+    {
+      title: "How to ask for help",
+      link: "https://neog.camp/guide/self-troubleshoot",
+    },
+    {
+      title: "Interview tips",
+      link: "https://neog.camp/qualifier/camp-interview-tips",
+    },
+    {
+      title: "Practice question",
+      link: "https://neog.camp/qualifier/camp-interview-practice",
+    },
+    {
+      title: "Syllabus",
+      link: "https://neog.camp/handbook/learnings-peek",
+    },
+  ],
+};
+
+const btnSearch = document.querySelector("#btn-search");
+const inputText = document.querySelector("#search-bar");
+
+btnSearch.addEventListener("click", (e) => {
+  e.preventDefault();
+  var inputValue = inputText.value;
+  var showTeams = "";
+  var showVideo = "";
+  var impLinks = "";
+  if (inputValue === "team") {
+    database[inputValue].forEach((each) => {
+      showTeams += `
+            <div class="team_card">
+                <img src="${each.img}" />
+                <div class="bio">
+                <h3>${each.title} <a href="${each.link}"><i class="fab fa-linkedin"></i></a> </h3>
+                <h6>${each.job}</h6>
+                <p>${each.des}</h6>
+                </div>
+            </div>`;
+      
+      document.querySelector(".video-section").style.display = "none";
+      document.querySelector(".show-link-data").style.display = "none";
+      document.querySelector(".team-section").style.display = "flex";
+      document.querySelector(".show-team-data").innerHTML = showTeams;
+      document.forms[0].reset()
+    });
+  } else if (inputValue === "levelzero") {
+    database[inputValue].forEach((each) => {
+      showVideo += `
+                <div class="viedo-card">
+                <a href="${each.link}" target="_blank"><img src="${each.img}" ></a>
+               <div class="bio">
+               <h3>${each.title}</h3>
+               <p>${each.des}</p>
+               </div>
+                </div>
+                </div>
+
+            `
+      document.querySelector(".show-link-data").style.display = "none";
+      document.querySelector(".team-section").style.display = "none";
+      document.querySelector(".video-section").style.display = "flex";
+      document.querySelector(".show-video-data").innerHTML = showVideo;
+      document.forms[0].reset()
+    })
+  } else if (inputValue === "links") {
+    database[inputValue].forEach((each) => {
+      impLinks += `
+
+            <div class="link-card">
+                <p>${each.title} <a href="${each.link}" target="_blank"><i class="fas fa-link"></i> </a></p>
+            </div>
+
+            `;
+      document.querySelector(".video-section").style.display = "none";
+      document.querySelector(".team-section").style.display = "none";
+      document.querySelector(".show-link-data").style.display = "flex";
+      document.querySelector(".show-link-data").innerHTML = impLinks;
+      document.forms[0].reset()
+    });
+  }
+});
